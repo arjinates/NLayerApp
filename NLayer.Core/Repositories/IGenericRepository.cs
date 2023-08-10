1f75ba041fcb6c15beac6194f7479d90044b0806
Arjin Ateş <arjinates@hotmail.com>
2023-08-10 20:26:46 +0300

    IGenericRepository added

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        IQueryable<T> GetAll(Expression<Func<T, bool>> exp);
+
+        //IQueryable donen seylerde veritabanına sorgu atilmaz, toList'e kadar olan tum kosullar
+        //memory'de birlestirilir ve toList ile birlikte tek seferde veritabanına gönderilir
+
+        // productRepository.Where(x=> x.Id>%).OrderBy.ToListAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> exp);
+        //Queryable kullanmamin sebebi henuz veritabanina gitmeden OrderBy gibi siralama yapabilmek icin
+        //meaning, uygulamanin daha performansli calismasi icin
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> exp);
+
+        Task AddAsync(T entity);//async programlamayi var olan threadlari bloklamamak icin kullaniyoruz
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        void Update(T entity); //update ve remove'un async'i yok cunku uzun suren islemler olmadiklari icin
+
+        void remove(T entity);
+
+        Task RemoveRange(IEnumerable<T> entities);
+    }
+}
