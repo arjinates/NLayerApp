47f0dad5945c15c274f882a68e2f81ee1393df00
Arjin Ateş <arjinates@hotmail.com>
2023-08-15 11:56:25 +0300

    added ProductController

---
+++ NLayer.API/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        public ProductsController(IService<Product> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var products = await _service.GetAllAsync();
+            var productDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productDtos));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            var productDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productDto));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            await _service.removeAsync(product);
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(200)); //remove ile update bir sey donmediginden NoContentDto donuyoruz
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            var product = await _service.AddAsync(_mapper.Map<Product>(productDto));
+            productDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(201,productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductDto productDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Product>(productDto));   //updateAsync bir sey dönmedigi icin sadece servis uzerinden degisikligi yaptik
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));    //client'e bir sey donmeyecegimiz icin mapping yapmaya da gerek yok
+        }                                                                               
+
+
+    }
+}
