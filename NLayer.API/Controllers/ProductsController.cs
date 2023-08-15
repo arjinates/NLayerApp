0ad64c2cba4f694f41d124c8d475ddd7b0614e9c
Arjin Ateş <arjinates@hotmail.com>
2023-08-15 18:12:10 +0300

    updated: ProductController with new ProductService added: ProductWithCategory endpoint added necessary service configs

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using NLayer.Core.DTOs;
 using NLayer.Core.Models;
 using NLayer.Core.Services;
@@ -10,53 +11,56 @@ namespace NLayer.API.Controllers
     public class ProductsController : CustomBaseController
     {
         private readonly IMapper _mapper;
-        private readonly IService<Product> _service;
+        private readonly IProductService _productService;
 
-        public ProductsController(IService<Product> service, IMapper mapper)
+        public ProductsController(IMapper mapper, IProductService productService)
         {
-            _service = service;
             _mapper = mapper;
+            _productService = productService;
         }
 
-        [HttpGet]
+        [HttpGet] // GET api/products
         public async Task<IActionResult> All()
         {
-            var products = await _service.GetAllAsync();
+            var products = await _productService.GetAllAsync();
             var productDtos = _mapper.Map<List<ProductDto>>(products.ToList());
             return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productDtos));
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}")] // GET api/products/5
         public async Task<IActionResult> GetById(int id)
         {
-            var product = await _service.GetByIdAsync(id);
+            var product = await _productService.GetByIdAsync(id);
             var productDto = _mapper.Map<ProductDto>(product);
             return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productDto));
+        }
+
+        [HttpGet("[action]")] // GET api/product/GetProductsWithCategory
+        public async Task<IActionResult> GetProductsWithCategory()
+        {
+            return CreateActionResult(await _productService.GetProductsWithCategory());
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Remove(int id)
         {
-            var product = await _service.GetByIdAsync(id);
-            await _service.removeAsync(product);
+            await _productService.removeAsync(await _productService.GetByIdAsync(id));
             return CreateActionResult(CustomResponseDto<NoContentDto>.Success(200)); //remove ile update bir sey donmediginden NoContentDto donuyoruz
         }
 
         [HttpPost]
         public async Task<IActionResult> Save(ProductDto productDto)
         {
-            var product = await _service.AddAsync(_mapper.Map<Product>(productDto));
+            var product = await _productService.AddAsync(_mapper.Map<Product>(productDto));
             productDto = _mapper.Map<ProductDto>(product);
             return CreateActionResult(CustomResponseDto<ProductDto>.Success(201,productDto));
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update(ProductDto productDto)
+        public async Task<IActionResult> Update(ProductUpdateDto productDto)
         {
-            await _service.UpdateAsync(_mapper.Map<Product>(productDto));   //updateAsync bir sey dönmedigi icin sadece servis uzerinden degisikligi yaptik
+            await _productService.UpdateAsync(_mapper.Map<Product>(productDto));   //updateAsync bir sey dönmedigi icin sadece servis uzerinden degisikligi yaptik
             return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));    //client'e bir sey donmeyecegimiz icin mapping yapmaya da gerek yok
         }                                                                               
-
-
     }
 }
