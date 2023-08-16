0225623326bffacc9be8ad84f81f23b625c37da8
Arjin Ateş <arjinates@hotmail.com>
2023-08-16 15:29:19 +0300

    added: category repositories and services and implementations

---
+++ NLayer.Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Category> GetCategoryByIdWithProductsAsync(int categoryId)
+        {
+           return await _context.Categories
+                .Include(c => c.Products).Where(x => x.Id == categoryId).SingleOrDefaultAsync();
+        }
+    }
+}
