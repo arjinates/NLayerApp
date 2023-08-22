dac329743d492edc3ea560ef625448c1bd37847d
Arjin Ateş <arjinates@hotmail.com>
2023-08-22 17:30:53 +0300

    added: ProductApiService added: HttpClients to program.cs

--- NLayer.Web/Program.cs
+++ NLayer.Web/Program.cs
@@ -8,6 +8,7 @@ using NLayer.Service.Mapping;
 using NLayer.Service.Validation;
 using NLayer.Web;
 using NLayer.Web.Modules;
+using NLayer.Web.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,6 +27,15 @@ builder.Services.AddDbContext<AppDbContext>(x =>
         option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
         //GeAssembly("NLayerApp.Repository") seklinde de yazabilirdik ama bu statik olurdu
     }); //dinamik typeOf AppDbContext name yaparak dinamiklestirdik
+});
+
+builder.Services.AddHttpClient<ProductApiService>(opt =>
+{
+    opt.BaseAddress = new Uri(builder.Configuration["BaseUrl"]);
+});
+builder.Services.AddHttpClient<CategoryApiService>(opt =>
+{
+    opt.BaseAddress = new Uri(builder.Configuration["BaseUrl"]);
 });
 
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
